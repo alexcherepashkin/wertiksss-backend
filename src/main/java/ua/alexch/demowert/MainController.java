53a18d19f7fab7412357c09f02137ae650ba7f8a
Alexey Cherepashkin <alex.cherepashkin@gmail.com>
2020-07-10 17:46:35 +0300

    add client-app

--- src/main/java/ua/alexch/demowert/MainController.java
+++ src/main/java/ua/alexch/demowert/MainController.java
@@ -3,6 +3,7 @@ package ua.alexch.demowert;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/")
-//@CrossOrigin(allowedHeaders = { "*" }, origins = { "*", "http://localhost:4200", "http://localhost:8080" })
+@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:8080", "*" })
 public class MainController {
     private final PersonService service;
 
@@ -24,38 +25,38 @@ public class MainController {
         this.service = service;
     }
 
-    @GetMapping
-    public String hello() {
-        return "Hello AlexCH!!";
-    }
+//    @GetMapping
+//    public String hello() {
+//        return "Hello Alex!!";
+//    }
 
-    @GetMapping("/persons/search")
+    @GetMapping("/accounts/search")
     public List<Person> findAllByTerm(@RequestParam(name = "term", required = false) String term,
             @RequestParam(name = "option", required = false) String option) {
         return service.findAllByTerm(term, option);
     }
 
-    @GetMapping("/persons/{id}")
+    @GetMapping("/accounts/{id}")
     public Person findById(@PathVariable("id") Long id) throws DomainException {
         return service.findById(id);
     }
 
-    @GetMapping("/persons")
+    @GetMapping("/accounts")
     public List<Person> findAll() {
         return service.findAll();
     }
 
-    @PostMapping("/persons")
+    @PostMapping("/accounts")
     public Person savePerson(@RequestBody Person person) throws DomainException {
         return service.savePerson(person);
     }
 
-    @PutMapping("/persons/{id}")
+    @PutMapping("/accounts/{id}")
     public Person updatePerson(@RequestBody Person person) throws DomainException {
         return service.updatePerson(person);
     }
 
-    @DeleteMapping("/persons/{id}")
+    @DeleteMapping("/accounts/{id}")
     public void removePerson(@PathVariable("id") Long id) throws DomainException {
         service.removePerson(id);
     }
